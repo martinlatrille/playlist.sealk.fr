795a83f16662119407fd5a8659a906a4307930cb
latril_m <martin.latrille@epita.fr>
2014-09-17 23:53:09 +0200

    sealk: init

--- playlist/wsgi.py
+++ playlist/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-#application = Cling(get_wsgi_application())
+#application = django.core.handlers.wsgi.WSGIHandler()
+application = Cling(get_wsgi_application())
