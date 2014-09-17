f17e406441e504e937e96fc46f50486ed56c6737
latril_m <martin.latrille@epita.fr>
2014-09-17 22:51:35 +0200

    sealk: init

--- playlist/urls.py
+++ playlist/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'playlist.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'^', include('main.urls')),
+    url(r'^admin', include(admin.site.urls)),
+		url(r'^', include('main.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
