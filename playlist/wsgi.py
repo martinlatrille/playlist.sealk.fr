3dfab89424a3681fa5bc5ce37ac0d73b19eef8fe
latril_m <martin.latrille@epita.fr>
2014-09-17 23:46:55 +0200

    sealk: init

--- playlist/wsgi.py
+++ playlist/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
+import sys
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playlist.settings")
 
